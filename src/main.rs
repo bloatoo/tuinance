73905be52460c98db4fc7d1922716d88bb670676
quasar <bloatoo@tuta.io>
2021-08-20 13:25:35 +0300

    extend graph capabilities

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,10 @@
-use tuinance::{
-    event::*,
-};
+use tuinance::event::*;
 
-use yahoo_finance::Streamer;
+use yahoo_finance::{history, Interval, Streamer, Timestamped};
 
 use ordered_float::OrderedFloat;
 
-use std::sync::mpsc;
+const TICKER: &str = "NTDOY";
 
 use tui::{
     backend::CrosstermBackend,
@@ -54,15 +52,20 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let backend = CrosstermBackend::new(stdout);
     let mut terminal = Terminal::new(backend)?;
 
+    let mut args = std::env::args();
+    args.next();
+
+    let ticker = args.next().unwrap_or("NTDOY".into()).to_uppercase();
+
     terminal.clear()?;
     let size = terminal.size()?;
 
     let events = Events::new(1000);
 
-    let streamer = Streamer::new(vec!["AAPL"]);
-    let (mut tx, rx) = mpsc::channel();
+    let _streamer = Streamer::new(vec![&ticker]);
+    //let (mut tx, rx) = mpsc::channel();
 
-    tokio::spawn(async move {
+    /*tokio::spawn(async move {
         streamer.stream().await
         .for_each(move |quote| {
             //tx.send(format!("At {}, {} is trading for ${}", quote.timestamp, quote.symbol, quote.price)).unwrap();
@@ -70,7 +73,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
             future::ready(())
         })
         .await;
-    });
+    });*/
 
 
     let mut chunks = Layout::default()
@@ -82,6 +85,17 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let mut data: Vec<OrderedFloat<f64>> = vec![];
 
+    let hist = history::retrieve_interval(&ticker, Interval::_max).await.unwrap();
+
+    let len = hist.len();
+
+    let mut y = vec![];
+
+    for d in hist.iter() {
+        data.push(OrderedFloat::from(d.high));
+        y.push(format!("{}", d.datetime().format("%b %e %Y")));
+    }
+
     loop {
         let floats: Vec<(f64, f64)> = data.iter().enumerate()
             .map(|(idx, &elem)| (idx as f64 + 1.0, f64::from(elem)))
@@ -92,9 +106,13 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         let min = f64::from(data.iter().min().unwrap_or(&p).clone());
         let max = f64::from(data.iter().max().unwrap_or(&p).clone());
 
+        let f_date = y.first().unwrap_or(&String::new()).to_string();
+        let m_date = y.get(y.len() / 2).unwrap_or(&String::new()).to_string();
+        let l_date = y.last().unwrap_or(&String::new()).to_string();
+
         let datasets = vec![
             Dataset::default()
-            .name("data1")
+            .name(&ticker)
                 .marker(symbols::Marker::Dot)
                 .graph_type(GraphType::Line)
                 .style(Style::default().fg(Color::Yellow))
@@ -102,15 +120,15 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         ];
 
         let chart = Chart::new(datasets)
-            .block(Block::default().title("Chart").borders(Borders::ALL))
+            .block(Block::default().title("Tuinance").borders(Borders::ALL))
             .x_axis(Axis::default()
-                .title(Span::styled("X Axis", Style::default().fg(Color::Red)))
-                .style(Style::default().fg(Color::White))
-                .bounds([0.0, 10.0])
-                .labels(["0.0", "5.0", "10.0"].iter().cloned().map(Span::from).collect()))
+                .title(Span::styled("Date", Style::default().fg(Color::Blue)))
+                .style(Style::default().fg(Color::Magenta))
+                .bounds([0.0, len as f64])
+                .labels([f_date, m_date, l_date].iter().cloned().map(|x| Span::styled(x, Style::default().fg(Color::Blue))).collect()))
             .y_axis(Axis::default()
-                .title(Span::styled("Y Axis", Style::default().fg(Color::Red)))
-                .style(Style::default().fg(Color::White))
+                .title(Span::styled("Price", Style::default().fg(Color::Blue)))
+                .style(Style::default().fg(Color::Magenta))
                 .bounds([min, max])
                 .labels([format!("{:.2}", min), format!("{:.2}", max)].iter().cloned().map(Span::from).collect()));
 
@@ -118,8 +136,8 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
             chunks = Layout::default()
                 .direction(Direction::Horizontal)
                 .constraints(vec![
-                    Constraint::Percentage(20),
-                    Constraint::Percentage(80)
+                    Constraint::Percentage(15),
+                    Constraint::Percentage(85)
                 ]).split(size);
         }
 
@@ -128,13 +146,13 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
             f.render_widget(Paragraph::new(Text::from(format!("{:#?}", data))).block(Block::default().title("Debug").borders(Borders::ALL)), chunks[0])
         })?;
 
-        if let Ok(f) = rx.try_recv() {
+        /*if let Ok(f) = rx.try_recv() {
             if data.len() > 10 {
                 data.remove(0);
             }
 
             data.push(OrderedFloat::from(f));
-        }
+        }*/
 
         if let Ok(ev) = events.next() {
             match ev {
